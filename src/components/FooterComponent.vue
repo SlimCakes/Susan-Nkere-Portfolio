c7848a09347b6be847b532ef343a0c5931abcb26
Susan Nkere <susan.nkere02@gmail.com>
2025-01-06 16:25:39 +0100

    jan 6th seventh commit

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative bg-[#DDb890] w-[1440px] min-h-[400px] flex flex-col justify-between">
+  <div class="relative bg-[#DDb890] max-w-[1440px] min-h-[400px] flex flex-col justify-between">
     <!-- Social Media Icons -->
     <div class="flex justify-end gap-6 items-center">
       <div class="hover:opacity-80">
@@ -18,6 +18,18 @@
       SUSAN NKERE
     </h1>
   </div>
+  <div class="flex justify-end gap-6 items-center">
+      <div class="hover:opacity-80">
+      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928366/u_linkedin_gmhekn.svg" alt="LinkedIn" class="w-[48px] h-[48px]"/>
+    </div>
+      <div class="hover:opacity-80">
+      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928402/u_instagram_kxbnkx.svg" alt="Instagram" class="w-[48px] h-[48px]"/>
+      </div>
+    <div class="hover:opacity-80">
+      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928349/u_behance-alt_xjmph6.svg" alt="Behance" class="w-[48px] h-[48px]"/>
+    </div>
+    </div>
+
 </template>
 
 <script setup>
