aecbde86d0141d8ef9bc910bbb6ef4c1231a908b
Susan Nkere <susan.nkere02@gmail.com>
2025-01-07 11:41:22 +0100

    jan 7th first commit

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,40 +1,43 @@
 <template>
-  <div class="relative bg-[#DDb890] max-w-[1440px] min-h-[400px] flex flex-col justify-between">
-    <!-- Social Media Icons -->
-    <div class="flex justify-end gap-6 items-center">
-      <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928366/u_linkedin_gmhekn.svg" alt="LinkedIn" class="w-[48px] h-[48px]"/>
-    </div>
-      <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928402/u_instagram_kxbnkx.svg" alt="Instagram" class="w-[48px] h-[48px]"/>
+  <div class="max-w-[1440px] h-[400px] mx-auto bg-[#DDB892]">
+    <div class="relative w-full h-full flex flex-col justify-between p-8">
+      <!-- Social Media Icons -->
+      <div class="flex justify-end gap-8 pt-4">
+        <a href="#" class="hover:opacity-80 transition-opacity">
+          <img 
+            src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928366/u_linkedin_gmhekn.svg" 
+            alt="LinkedIn" 
+            class="w-[32px] h-[32px]"
+          />
+        </a>
+        <a href="#" class="hover:opacity-80 transition-opacity">
+          <img 
+            src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928402/u_instagram_kxbnkx.svg" 
+            alt="Instagram" 
+            class="w-[32px] h-[32px]"
+          />
+        </a>
+        <a href="#" class="hover:opacity-80 transition-opacity">
+          <img 
+            src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928349/u_behance-alt_xjmph6.svg" 
+            alt="Behance" 
+            class="w-[32px] h-[32px]"
+          />
+        </a>
       </div>
-    <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928349/u_behance-alt_xjmph6.svg" alt="Behance" class="w-[48px] h-[48px]"/>
-    </div>
-    </div>
 
-    <!-- Name Text -->
-    <h1 class="text-white font-playfair text-[120px] leading-none tracking-wider">
-      SUSAN NKERE
-    </h1>
+      <!-- Name Image -->
+      <div class="pb-8">
+        <img 
+          src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1736246352/image_43_vzcbcw.png"
+          alt="Susan Nkere"
+          class="w-auto h-[120px]" 
+        />
+      </div>
+    </div>
   </div>
-  <div class="flex justify-end gap-6 items-center">
-      <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928366/u_linkedin_gmhekn.svg" alt="LinkedIn" class="w-[48px] h-[48px]"/>
-    </div>
-      <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928402/u_instagram_kxbnkx.svg" alt="Instagram" class="w-[48px] h-[48px]"/>
-      </div>
-    <div class="hover:opacity-80">
-      <img src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928349/u_behance-alt_xjmph6.svg" alt="Behance" class="w-[48px] h-[48px]"/>
-    </div>
-    </div>
-
 </template>
 
 <script setup>
-// Add any component logic here if needed
+// No additional logic needed
 </script>
-
-<style>
-</style>
