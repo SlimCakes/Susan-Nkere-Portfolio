b34650b09740888467cc9a6cde8815dd1049809c
Susan Nkere <susan.nkere02@gmail.com>
2025-01-22 14:51:02 +0100

    first commit 22nd jan

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <!-- App.vue -->
+<!-- App.vue -->
 <template>
   <div class="flex flex-col min-h-screen">
     <NavBarComponent class="w-full" />
-    <main class="flex-1 flex flex-col">
-      <HeroComponent />
-      <AboutComponent />
-      <WorkExperienceComponent />
-      <PortfolioComponent />
-      <FooterComponent />
-    </main>
+      <main class="flex-1 mx-auto px-4">
+        <HeroComponent />
+        <AboutComponent />
+        <WorkExperienceComponent />
+        <PortfolioComponent />
+      </main>
+      <footer class="w-[100vw] bg-[#DDB892] h-[247px] relative left-[41%] right-[50%] -mx-[50vw]">
+        <FooterComponent />
+    </footer>
   </div>
 </template>
 
 <script>
+import NavBarComponent from './components/NavBarComponent.vue'
 import HeroComponent from '@/components/HeroComponent.vue'
 import AboutComponent from '@/components/AboutComponent.vue'
 import PortfolioComponent from '@/components/PortfolioComponent.vue'
-import NavBarComponent from './components/NavBarComponent.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
 import WorkExperienceComponent from './components/WorkExperienceComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 
 export default {
   name: 'App',
@@ -34,9 +37,10 @@ export default {
 </script>
 
 <style>
-body {
+body{
   margin: 0;
   padding: 0;
+  overflow-x: hidden;
 }
 
 #app {
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,44 +1,41 @@
 <template>
-  <div class="max-w-auto bg-[#DDB892]">
-    <div class="w-full h-[400px] mx-auto">
-      <div class="relative w-full h-full flex flex-col justify-between p-8">
+  <div class="w-full" >
+    <div class="flex flex-col justify-between">
         <!-- Social Media Icons -->
         <div class="flex justify-end gap-8 pt-4">
-          <a href="#" class="hover:opacity-80 transition-opacity">
+          <a class="hover:opacity-80 transition-opacity">
             <img 
               src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928366/u_linkedin_gmhekn.svg" 
               alt="LinkedIn" 
               class="w-[32px] h-[32px]"
             />
           </a>
-          <a href="#" class="hover:opacity-80 transition-opacity">
+          <a class="hover:opacity-80 transition-opacity">
             <img 
               src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928402/u_instagram_kxbnkx.svg" 
               alt="Instagram" 
-              class="w-[32px] h-[32px]"
-            />
+              class="w-[32px] h-[32px]"/>
           </a>
-          <a href="#" class="hover:opacity-80 transition-opacity">
+          <a class="hover:opacity-80 transition-opacity">
             <img 
               src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1735928349/u_behance-alt_xjmph6.svg" 
               alt="Behance" 
-              class="w-[32px] h-[32px]"
-            />
+              class="w-[32px] h-[32px]"/>
           </a>
         </div>
-
+        <div class=" pl-[40px] w-[923px] h-[2px] bg-[#E0E0E0]"></div>
         <!-- Name Image -->
-        <div class="pb-8">
+        <div class="pt-[135px] flex justify-center">
           <img 
             src="https://res.cloudinary.com/dxgoaa5mn/image/upload/v1736246352/image_43_vzcbcw.png"
             alt="Susan Nkere"
-            class="w-auto h-[120px]" 
+            class="w-[850.5px] h-[57px]" 
           />
         </div>
       </div>
-    </div>
   </div>
 </template>
 
-<script setup>
+<script>
 </script>
+
