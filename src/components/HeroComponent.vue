6f9b7331f393c3797c5060e30030c336bc3b5b9e
Susan Nkere <susan.nkere02@gmail.com>
2025-01-29 13:27:23 +0100

    first commit 29th jan

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -5,25 +5,25 @@
       <!-- Hero section -->
       <div class="grid grid-cols-2 md:grid-cols-2 items-center gap-8">
   <!-- Left Column: UX/UI Designer, Portfolio, Horizontal Line, and Name -->
-  <div class="flex flex-col">
-    <div class="w-[1000px] h-[309px]">
-  <p class="text-[48px] font-secondary font-normal text-[#333333]">UX/UI Designer</p>
-  <h1 class="text-[128px] font-primary font-semibold text-[#333333]">Portfolio</h1>
+  <div class="flex flex-col items-center md:items-start text-center md:text-left">
+    <div class="md:w-[1000px] h-auto md:h-[309px]">
+  <p class="text-[24px] md:text-[48px]  font-secondary font-normal text-[#333333]">UX/UI Designer</p>
+  <h1 class="text-[48px] md:text-[128px]  font-primary font-semibold text-[#333333]">Portfolio</h1>
 </div>
-    <div class="flex items-center justify-start w-[1144px] mt-[100px]">
-      <div class="w-[923px] h-[4px] bg-[#DBC1AC]"></div>
-      <span class="ml-4 text-[24px] font-secondary text-[#333333]">Susan Nkere</span>
+  <div class="flex items-center justify-center md:justify-start w-[350px] ml-[24px] md:w-[1144px] mt-[20px] md:mt-[100px]">
+      <div class="w-[100px] md:w-[923px] h-[4px] bg-[#DBC1AC]"></div>
+      <span class="ml-4 text-[16px] md:text-[24px] font-secondary text-[#333333]">Susan Nkere</span>
     </div>
   </div>
 
    <!-- Right Column: Vertical Line and Date -->
-   <div class="absolute right-0 flex flex-col items-start mt-[20px] h-[592px] mr-[108px]">
-    <span class="mt-4 -rotate-90 origin-left transform translate-y-[-10px] text-[24px] font-secondary w-[135px]">2023-2024</span>
-    <div class="w-[4px] h-[437px] bg-[#DBC1AC]"></div>
-  </div>
-</div>
-          </main>
-        </section>
+   <div class="flex flex-col items-center md:items-start mt-[20px] md:mt-[20px] h-auto md:h-[592px]">
+      <span class="text-[16px] md:text-[24px] font-secondary rotate-0 md:-rotate-180 transform md:translate-y-[-10px]">2023-2024</span>
+      <div class="w-[4px] h-[100px] md:h-[437px] bg-[#DBC1AC] mt-[20px] md:mt-[0]"></div>
+    </div>
+    </div>
+      </main>
+    </section>
 </template>
 
 <style scoped>
